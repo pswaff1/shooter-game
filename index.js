60dd5721584543867d17bd91bf3ea814cddc0ab3
Patrick Swafford <patrick.swafford@swaffordhouse.com>
2023-12-20 19:55:26 -0500

    added player.js, fleshed out other files

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert("test");
+window.addEventListener('load', function() {
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+    class Game {
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        update () {
+        
+        }
+
+        draw () {
+            
+        }
+    }
+});
